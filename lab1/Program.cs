d54c1e0074da48035a6eb39f5c2f3cb0f1bdb9f1
Jwindustries <johnwhitefootball@gmail.com>
2025-03-18 22:53:56 +0000

    Finished and tested

---
+++ lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 1
+        Console.WriteLine("Johnathon White IT 1050 CRN 13076");
+        // Step 2 
+        /* Name : Johnathon White
+         * Title : IT 1050 Lab 1
+         */
+        //Step 3
+        int myFavNum = 262;
+            Console.WriteLine("My favorite number is " + myFavNum + " which is the number of cubic inches in a ninties four point three liter Chevy V six engine.");
+        string myFavProgLang = "C#";
+            Console.WriteLine("My favorite programing language is " + myFavProgLang + ".");
+        double NumOfLangsWrittenBefore = 0.0;
+            Console.WriteLine("The number of programs I have written before this is " + NumOfLangsWrittenBefore + ".");
+        bool MyPreviousProgrammingExp = false;
+            Console.WriteLine("I have previous programming experience = " + MyPreviousProgrammingExp + ".");
+        //Step 4 Where did it go?
+        //Step 5
+        const string myCollegeName = "Cuyahoga Community College";
+        Console.WriteLine("The college I attend is " + myCollegeName + ".");
+
+
+
+
+
+
+    }
+}
