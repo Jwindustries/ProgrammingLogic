693eaf5b5cc35d66fb237046514e2a3cd5621cd8
Jwindustries <johnwhitefootball@gmail.com>
2025-04-28 17:17:04 +0000

    done

--- Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2991f53d635920da4b9fd8f27e2b180df42125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c5f748b1e1d5e8238de94ebf380bd986c5b108")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab6/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+
+class Car
+{
+  public string model;
+  public string color;
+  public int year;
+
+  public void Start()
+  {
+    Console.WriteLine("The Car is starting");
+  }
+
+  public void Drive(int miles)
+  {
+    Console.WriteLine($"The Car Drove {miles} miles");
+  }
+
+  public string GetDescription()
+  {
+    return $"{year} {color} {model}";
+
+  }
+
+  public void Repaint(string newColor)
+  {
+    color = newColor;
+    Console.WriteLine($"The car has been repainted to {newColor}");
+  }
+
+  // Example method to display car details. You can use this as an example for the other methods.
+  public void Display()
+  {
+    Console.WriteLine("Car Details:");
+    Console.WriteLine("Model: " + model);
+    Console.WriteLine("Color: " + color);
+    Console.WriteLine("Year: " + year);
+  }
+  
+  
+}
+
+
+class Program
+{
+  static void Main()
+  {
+    // Creating a Car object and assigning values to its properties
+    Car myCar = new Car();
+    myCar.model = "Civic";
+    myCar.color = "Black";
+    myCar.year = 2020;
+
+    myCar.Display(); // Calling the Display method
+
+    myCar.Start();   //Calling the Start method   
+
+    myCar.Drive(50); //Calling the Drive Method with parameter of (50)
+
+    String carDescription = myCar.GetDescription();
+    Console.WriteLine(carDescription);
+
+    myCar.Repaint("Red");
+    
+
+  }
+
+}
